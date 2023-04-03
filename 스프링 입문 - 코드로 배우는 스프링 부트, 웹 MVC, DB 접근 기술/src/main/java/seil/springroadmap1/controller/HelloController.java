3abcf9d72b09dd4fc0f33f2dc8ec662e00270d1d
ohseil <sioh0612@gmail.com>
2023-04-03 21:04:22 +0900

    MVC + Template Engine을 이용한 동적 컨텐츠 제공 방식 구현

--- 스프링 입문 - 코드로 배우는 스프링 부트, 웹 MVC, DB 접근 기술/src/main/java/seil/springroadmap1/controller/HelloController.java
+++ 스프링 입문 - 코드로 배우는 스프링 부트, 웹 MVC, DB 접근 기술/src/main/java/seil/springroadmap1/controller/HelloController.java
@@ -3,13 +3,20 @@ package seil.springroadmap1.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
 
     @GetMapping("hello")
-    public String hello(Model model) {
+    public String hello(final Model model) {
         model.addAttribute("data", "hello!!!!");
         return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") final String name, final Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
     }
 }
