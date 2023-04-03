f124cfec47419bc0d131b3a708a12a2fe0771e1b
ohseil <sioh0612@gmail.com>
2023-04-03 22:33:00 +0900

    회원 관리 예제 시작. 회원 Domain, Repository 구현

---
+++ 스프링 입문 - 코드로 배우는 스프링 부트, 웹 MVC, DB 접근 기술/src/main/java/seil/springroadmap1/repository/MemberRepository.java
@@ -0,0 +1,13 @@
+package seil.springroadmap1.repository;
+
+import seil.springroadmap1.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+    Member save(Member member);
+    Optional<Member> findById(Long id);
+    Optional<Member> findByName(String name);
+    List<Member> findAll();
+}
---
+++ 스프링 입문 - 코드로 배우는 스프링 부트, 웹 MVC, DB 접근 기술/src/main/java/seil/springroadmap1/repository/MemoryMemberRepository.java
@@ -0,0 +1,36 @@
+package seil.springroadmap1.repository;
+
+import seil.springroadmap1.domain.Member;
+
+import java.util.*;
+
+public class MemoryMemberRepository implements MemberRepository {
+
+    // 실제로는 동시성 문제를 고려해야 하지만 여기서는 예제이기 때문에 하지 않는다.
+    private static Map<Long, Member> store = new HashMap<>();
+    private static long sequence = 0L;
+
+    @Override
+    public Member save(Member member) {
+        member.setId(++sequence);
+        store.put(member.getId(), member);
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        return Optional.ofNullable(store.get(id));
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        return store.values().stream()
+                .filter(member -> member.getName().equals(name))
+                .findAny();
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values());
+    }
+}
