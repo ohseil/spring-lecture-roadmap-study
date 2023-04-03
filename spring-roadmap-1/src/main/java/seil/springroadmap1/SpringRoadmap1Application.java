0b3783dc6fd3f24b4ab243d7b367d120de34cf6b
ohseil <sioh0612@gmail.com>
2023-04-03 19:37:52 +0900

    Roadmap 1 프로젝트 시작

---
+++ spring-roadmap-1/src/main/java/seil/springroadmap1/SpringRoadmap1Application.java
@@ -0,0 +1,13 @@
+package seil.springroadmap1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRoadmap1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRoadmap1Application.class, args);
+	}
+
+}
---
+++ spring-roadmap-1/src/test/java/seil/springroadmap1/SpringRoadmap1ApplicationTests.java
@@ -0,0 +1,13 @@
+package seil.springroadmap1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRoadmap1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
