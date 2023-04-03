79a27191546cc56ff7046ef1e28ef3286786c40a
ohseil <sioh0612@gmail.com>
2023-04-03 22:14:25 +0900

    API 구현

--- 스프링 입문 - 코드로 배우는 스프링 부트, 웹 MVC, DB 접근 기술/src/main/java/seil/springroadmap1/controller/HelloController.java
+++ 스프링 입문 - 코드로 배우는 스프링 부트, 웹 MVC, DB 접근 기술/src/main/java/seil/springroadmap1/controller/HelloController.java
@@ -4,19 +4,49 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
 
+    // 정적 데이터 반환
     @GetMapping("hello")
     public String hello(final Model model) {
         model.addAttribute("data", "hello!!!!");
         return "hello";
     }
 
+    // MVC + Temlate Engine을 이용한 동적 데이터 반환
     @GetMapping("hello-mvc")
     public String helloMvc(@RequestParam("name") final String name, final Model model) {
         model.addAttribute("name", name);
         return "hello-template";
+    }
+
+    // API로 일반 문자열을 바로 Response Body에 담아 반환
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") final String name) {
+        return "hello " + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") final String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
