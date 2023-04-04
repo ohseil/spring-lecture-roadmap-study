76fe0a8a9f3de118de108708311a655c75b012bf
ohseil <sioh0612@gmail.com>
2023-04-04 15:16:00 +0900

    H2와 JDBC를 사용해서 DAO 구현

--- 스프링 입문 - 코드로 배우는 스프링 부트, 웹 MVC, DB 접근 기술/src/main/java/seil/springroadmap1/SpringConfig.java
+++ 스프링 입문 - 코드로 배우는 스프링 부트, 웹 MVC, DB 접근 기술/src/main/java/seil/springroadmap1/SpringConfig.java
@@ -2,15 +2,24 @@ package seil.springroadmap1;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import seil.springroadmap1.repository.JdbcMemberRepository;
 import seil.springroadmap1.repository.MemberRepository;
 import seil.springroadmap1.repository.MemoryMemberRepository;
 import seil.springroadmap1.service.MemberService;
+
+import javax.sql.DataSource;
 
 /**
  * Java 코드로 빈을 등록하는 방법
  */
 @Configuration
 public class SpringConfig {
+
+    private final DataSource dataSource;
+
+    public SpringConfig(final DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -19,6 +28,6 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
