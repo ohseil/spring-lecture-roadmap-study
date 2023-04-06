6338ad48f0f88cfca9377e87ca6c6d7e902c6868
ohseil <sioh0612@gmail.com>
2023-04-06 17:03:21 +0900

    스프링 핵심 원리 - 기본편 프로젝트 생성

---
+++ 스프링 핵심 원리 - 기본편/src/main/java/seil/springroadmap2/Springroadmap2Application.java
@@ -0,0 +1,13 @@
+package seil.springroadmap2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springroadmap2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springroadmap2Application.class, args);
+	}
+
+}
---
+++ 스프링 핵심 원리 - 기본편/src/test/java/seil/springroadmap2/Springroadmap2ApplicationTests.java
@@ -0,0 +1,13 @@
+package seil.springroadmap2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springroadmap2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
