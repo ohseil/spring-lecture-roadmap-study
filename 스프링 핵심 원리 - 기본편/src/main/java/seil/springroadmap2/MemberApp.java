9d87e58436aba74350d32b32391d62f31b2aa598
ohseil <sioh0612@gmail.com>
2023-04-07 05:02:36 +0900

    회원 도메인 테스트 코드 작성

---
+++ 스프링 핵심 원리 - 기본편/src/main/java/seil/springroadmap2/MemberApp.java
@@ -0,0 +1,19 @@
+package seil.springroadmap2;
+
+import seil.springroadmap2.member.DefaultMemberService;
+import seil.springroadmap2.member.Grade;
+import seil.springroadmap2.member.Member;
+import seil.springroadmap2.member.MemberService;
+
+public class MemberApp {
+
+    public static void main(String[] args) {
+        MemberService memberService = new DefaultMemberService();
+        Member member = new Member(1L, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Member findMember = memberService.findMember(1L);
+        System.out.println("new member = " + member.getName());
+        System.out.println("find Member = " + findMember.getName());
+    }
+}
