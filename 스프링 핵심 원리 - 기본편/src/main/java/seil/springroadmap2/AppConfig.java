fb7187073e305be2d9306dde97b2afb4d3d4d570
ohseil <sioh0612@gmail.com>
2023-04-07 16:27:22 +0900

    AppConfig 리팩터링, 구현체를 선택해서 객체 생성하는 method를 추출

--- 스프링 핵심 원리 - 기본편/src/main/java/seil/springroadmap2/AppConfig.java
+++ 스프링 핵심 원리 - 기본편/src/main/java/seil/springroadmap2/AppConfig.java
@@ -1,7 +1,9 @@
 package seil.springroadmap2;
 
+import seil.springroadmap2.discount.DiscountPolicy;
 import seil.springroadmap2.discount.FixDiscountPolicy;
 import seil.springroadmap2.member.DefaultMemberService;
+import seil.springroadmap2.member.MemberRepository;
 import seil.springroadmap2.member.MemberService;
 import seil.springroadmap2.member.MemoryMemberRepository;
 import seil.springroadmap2.order.DefaultOrderService;
@@ -10,10 +12,18 @@ import seil.springroadmap2.order.OrderService;
 public class AppConfig {
 
     public MemberService memberService() {
-        return new DefaultMemberService(new MemoryMemberRepository());
+        return new DefaultMemberService(memberRepository());
     }
 
     public OrderService orderService() {
-        return new DefaultOrderService(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new DefaultOrderService(memberRepository(), discountPolicy());
+    }
+
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    private DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 }
