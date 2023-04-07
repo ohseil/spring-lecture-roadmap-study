78c8539dddd1233672f3a28da103f1afe6272a9f
ohseil <sioh0612@gmail.com>
2023-04-08 04:20:22 +0900

    Bean 메타 정보인 BeanDefinition 확인하는 테스트 코드 작성

---
+++ 스프링 핵심 원리 - 기본편/src/test/java/seil/springroadmap2/beandefinition/BeanDefinitionTest.java
@@ -0,0 +1,28 @@
+package seil.springroadmap2.beandefinition;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+import seil.springroadmap2.AppConfig;
+
+public class BeanDefinitionTest {
+
+    //AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+    GenericXmlApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
+
+    @DisplayName("빈 설정 메타정보 확인")
+    @Test
+    void findApplicationBean() {
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+
+        for (String beanDefinitionName : beanDefinitionNames) {
+            BeanDefinition beanDefinition = ac.getBeanDefinition(beanDefinitionName);
+
+            if (beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION) {
+                System.out.println("beanDefinitionName = " + beanDefinitionName + " beanDefinition = " + beanDefinition);
+            }
+        }
+    }
+}
