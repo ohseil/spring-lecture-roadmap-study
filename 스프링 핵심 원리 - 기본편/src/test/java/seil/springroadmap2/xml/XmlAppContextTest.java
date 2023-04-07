4a230dfb40d788d43d9debdffe4858b3f8d1e9d9
ohseil <sioh0612@gmail.com>
2023-04-07 19:03:43 +0900

    XML 방식의 빈 설정 구현, 테스트 코드 작성

---
+++ 스프링 핵심 원리 - 기본편/src/test/java/seil/springroadmap2/xml/XmlAppContextTest.java
@@ -0,0 +1,18 @@
+package seil.springroadmap2.xml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+import seil.springroadmap2.member.MemberService;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class XmlAppContextTest {
+
+    @Test
+    void xmlAppContext() {
+        ApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberService.class);
+    }
+}
