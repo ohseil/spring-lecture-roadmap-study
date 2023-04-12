74ff2d3907fab30aebaebefe739f889f6988f1cd
ohseil <sioh0612@gmail.com>
2023-04-12 15:57:43 +0900

    Lombok 사용 코드 추가

---
+++ 스프링 핵심 원리 - 기본편/src/main/java/seil/springroadmap2/HelloLombok.java
@@ -0,0 +1,19 @@
+package seil.springroadmap2;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class HelloLombok {
+
+    private String name;
+    private int age;
+
+    public static void main(String[] args) {
+        HelloLombok helloLombok = new HelloLombok();
+        helloLombok.setName("asdf");
+
+        System.out.println("helloLombok = " + helloLombok);
+    }
+}
--- 스프링 핵심 원리 - 기본편/src/main/java/seil/springroadmap2/order/DefaultOrderService.java
+++ 스프링 핵심 원리 - 기본편/src/main/java/seil/springroadmap2/order/DefaultOrderService.java
@@ -1,22 +1,17 @@
 package seil.springroadmap2.order;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import seil.springroadmap2.discount.DiscountPolicy;
-import seil.springroadmap2.discount.FixDiscountPolicy;
 import seil.springroadmap2.member.Member;
 import seil.springroadmap2.member.MemberRepository;
-import seil.springroadmap2.member.MemoryMemberRepository;
 
 @Component
+@RequiredArgsConstructor
 public class DefaultOrderService implements OrderService {
 
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
-
-    public DefaultOrderService(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
 
     @Override
     public Order createOrder(final Long memberId, final String itemName, final int itemPrice) {
