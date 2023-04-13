4c97309da86ba3ad057ad37c91f148120ab19bd3
ohseil <sioh0612@gmail.com>
2023-04-13 23:58:13 +0900

    Bean 생명 주기 테스트 코드 작성

---
+++ 스프링 핵심 원리 - 기본편/src/test/java/seil/springroadmap2/lifecycle/BeanLifeCycleTest.java
@@ -0,0 +1,28 @@
+package seil.springroadmap2.lifecycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+public class BeanLifeCycleTest {
+
+    @Test
+    public void lifeCycleTest() {
+        ConfigurableApplicationContext ac = new AnnotationConfigApplicationContext(LifeCycleConfig.class);
+        NetworkClient networkClient = ac.getBean(NetworkClient.class);
+        ac.close();
+    }
+
+    @Configuration
+    static class LifeCycleConfig {
+
+        @Bean(initMethod = "init", destroyMethod = "close")
+        public NetworkClient networkClient() {
+            NetworkClient networkClient = new NetworkClient();
+            networkClient.setUrl("https://hello-spring.dev");
+            return networkClient;
+        }
+    }
+}
