6e9230afa2edb22fc9173c8616ebe0af76e730ba
ohseil <sioh0612@gmail.com>
2023-04-15 01:22:30 +0900

    빈 스코프 관련 테스트 코드 작성

---
+++ 스프링 핵심 원리 - 기본편/src/test/java/seil/springroadmap2/scope/SingletonWithPrototypeTest1.java
@@ -0,0 +1,98 @@
+package seil.springroadmap2.scope;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.inject.Provider;
+import java.lang.annotation.Annotation;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SingletonWithPrototypeTest1 {
+
+    @Test
+    void prototypeFind() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(PrototypeBean.class);
+
+        PrototypeBean prototypeBean1 = ac.getBean(PrototypeBean.class);
+        prototypeBean1.addCount();
+        assertThat(prototypeBean1.getCount()).isEqualTo(1);
+
+        PrototypeBean prototypeBean2 = ac.getBean(PrototypeBean.class);
+        prototypeBean2.addCount();
+        assertThat(prototypeBean2.getCount()).isEqualTo(1);
+    }
+
+    @Test
+    void singletonClientUserPrototype() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(
+                ClientBean.class, PrototypeBean.class
+        );
+
+        ClientBean clientBean1 = ac.getBean(ClientBean.class);
+        int count1 = clientBean1.logic();
+        assertThat(count1).isEqualTo(1);
+
+        ClientBean clientBean2 = ac.getBean(ClientBean.class);
+        int count2 = clientBean2.logic();
+        assertThat(count2).isEqualTo(1);
+    }
+
+    @Scope("singleton")
+    static class ClientBean {
+
+        // 스프링 컨테이너를 주입 받아 사용하는 방식
+        /*private final PrototypeBean prototypeBean; // 생성 시점에 주입
+
+        public ClientBean(final PrototypeBean prototypeBean) {
+            this.prototypeBean = prototypeBean;
+        }*/
+
+        // 스프링에서 제공하는 ObjectProvider 사용
+        /*private final ObjectProvider<PrototypeBean> prototypeBeanProvider;
+
+        public ClientBean(final ObjectProvider<PrototypeBean> prototypeBeanProvider) {
+            this.prototypeBeanProvider = prototypeBeanProvider;
+        }*/
+
+        // Java에서 제공하는 Java 표준인 Provider 사용
+        private final Provider<PrototypeBean> prototypeBeanProvider;
+
+        public ClientBean(final Provider<PrototypeBean> prototypeBeanProvider) {
+            this.prototypeBeanProvider = prototypeBeanProvider;
+        }
+
+        public int logic() {
+            PrototypeBean prototypeBean = prototypeBeanProvider.get();
+            prototypeBean.addCount();
+            return prototypeBean.getCount();
+        }
+    }
+
+    @Scope("prototype")
+    static class PrototypeBean {
+
+        private int count = 0;
+
+        public void addCount() {
+            count++;
+        }
+        public int getCount() {
+            return count;
+        }
+
+        @PostConstruct
+        public void init() {
+            System.out.println("PrototypeBean.init");
+        }
+
+        @PreDestroy
+        public void destroy() {
+            System.out.println("PrototypeBean.destroy");
+        }
+    }
+}
